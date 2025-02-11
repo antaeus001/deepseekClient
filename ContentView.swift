a4c76fecbe4502d8943371822a3fe6d4c0d141b1
antaeus001 <wushengwuxi01@163.com>
2025-02-11 15:43:58 +0800

    init

---
+++ ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  deepseekClient
+//
+//  Created by antaeus on 2025/2/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ deepseekClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  deepseekClientApp.swift
+//  deepseekClient
+//
+//  Created by antaeus on 2025/2/11.
+//
+
+import SwiftUI
+
+@main
+struct deepseekClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
